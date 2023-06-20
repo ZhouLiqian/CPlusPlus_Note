3e536e8370fe6d84f2d8811dbf4d7539e8b1abd5
ZhouLiqian <96431886+ZhouLiqian@users.noreply.github.com>
2023-06-20 22:35:59 +0800

    add code

--- leetcode/leetcode/code15.cpp
+++ leetcode/leetcode/code15.cpp
@@ -4,12 +4,14 @@
 //  Created by Qian on 6/20/23.
 
 #include <iostream>
+#include <string>
 using namespace::std;
 
 /*输入一个英文句子，翻转句子中单词的顺序，但单词内字符的顺序不变。为简单起见，标点符号和普通字母一样处理。例如输入字符串"I am a student. "，则输出"student. a am I"*/
 
 class Solution {
 public:
+    //方法一
     string reverseWords(string s) {
         // 反转整个字符串
         reverse(s.begin(), s.end());
@@ -18,11 +20,13 @@ public:
         for (int start = 0; start < n; ++start) {
             if (s[start] != ' ') {
                 // 填一个空白字符然后将idx移动到下一个单词的开头位置
-                if (idx != 0) s[idx++] = ' ';
+                if (idx != 0)
+                    s[idx++] = ' ';
 
                 // 循环遍历至单词的末尾
                 int end = start;
-                while (end < n && s[end] != ' ') s[idx++] = s[end++];
+                while (end < n && s[end] != ' ')
+                    s[idx++] = s[end++];
 
                 // 反转整个单词
                 reverse(s.begin() + idx - (end - start), s.begin() + idx);
