42faff60f5ba4cdaece42574485cfdd5f6d1062b
ZhouLiqian <96431886+ZhouLiqian@users.noreply.github.com>
2023-06-29 16:55:40 +0800

    add code

---
+++ leetcode/leetcode/Vector/code31.cpp
@@ -0,0 +1,44 @@
+//  code31.cpp
+//  leetcode
+//  顺时针打印矩阵
+//  Created by Qian on 6/29/23.
+
+#include <iostream>
+using namespace::std;
+
+/*输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字*/
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int start_row = 0, start_column = 0;
+        int end_row = matrix.size() - 1, end_column = matrix[0].size() - 1;
+        vector<int> cout_data;
+        if (matrix.empty()) {
+                return cout_data;
+            }
+        while (start_row <= end_row && start_column <= end_column) {
+            for(int i = start_column; i <= end_column; ++i){
+                cout_data.push_back(matrix[start_row][i]);
+            }
+            ++start_row;
+            for(int j = start_row; j <= end_row; ++j){
+                cout_data.push_back(matrix[j][end_column]);
+            }
+            --end_column;
+            if(start_row <= end_row){
+                for(int k = end_column; k >= start_column; --k){
+                    cout_data.push_back(matrix[end_row][k]);
+                }
+                --end_row;
+            }
+            if(start_column <= end_column){
+                for(int m = end_row; m >= start_row; --m){
+                    cout_data.push_back(matrix[m][start_column]);
+                }
+                ++start_column;
+            }
+        }
+        return cout_data;
+    }
+};
---
+++ leetcode/leetcode/Vector/code32.cpp
@@ -0,0 +1,29 @@
+//  code32.cpp
+//  leetcode
+//  数组中重复的数字
+//  Created by Qian on 6/29/23.
+
+#include <iostream>
+#include <unordered_set>
+using namespace::std;
+
+/*
+ 在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内
+ 数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次
+ 请找出数组中任意一个重复的数字
+ 解:无序哈希表-无序集合
+*/
+
+class Solution{
+public:
+    int findRepeatNumber(vector<int>& nums){
+        unordered_set<int> unique_set;
+        for(int num : nums){
+            if(unique_set.count(num) > 0){
+                return num;
+            }
+            unique_set.insert(num);
+        }
+        return -1;
+    }
+};
