533a54bb224c61bc768692e810bc3fb41707bef2
ZhouLiqian <96431886+ZhouLiqian@users.noreply.github.com>
2023-07-14 22:01:01 +0800

    add code

---
+++ leetcode/leetcode/Linklist/code74.cpp
@@ -0,0 +1,22 @@
+//  code74.cpp
+//  leetcode
+//  从尾到头打印链表
+//  Created by Qian on 7/14/23.
+
+#include <iostream>
+using namespace::std;
+
+/*输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）*/
+
+struct ListNode{
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL){}
+};
+
+class Solution {
+public:
+    vector<int> reversePrint(ListNode* head) {
+
+    }
+};
--- leetcode/leetcode/String/code2.cpp
+++ leetcode/leetcode/String/code2.cpp
@@ -7,87 +7,101 @@
 #include <iostream>
 using namespace::std;
 
-int strToInt(string str) {
-    int i = 0;
-    int num = 0;
-    int last_num = num;
+class Solution {
+public:
+    //整数
+    bool Integer(char c){
+        return c >= '0' && c <= '9';
+    }
+    //字符串转整数
+    int strToInt(string str) {
+        if(str.empty()) return 0;
+        int i = 0;
+        while(str[i] == ' ') i++;
+        int number = 0;
+        if(str[i] == '+'){
+            i++;
+            while(Integer(str[i])){
+                if(number < INT_MAX / 10 || (number == INT_MAX / 10 && (str[i] - '0') <= INT_MAX % 10)){
+                    number = number * 10 + (str[i] - '0');
+                    i++;
+                }
+                else{
+                    return INT_MAX;
+                }
+            }
+        }
+        else if(str[i] == '-'){
+            i++;
+            while(Integer(str[i])){
+                if(number > INT_MIN / 10 || (number == INT_MIN / 10 && - (str[i] - '0') >= INT_MIN % 10)){
+                    number = number * 10 - (str[i] - '0');
+                    i++;
+                }
+                else{
+                    return INT_MIN;
+                }
+            }
+        }
+        else{
+            if(Integer(str[i])){
+                while(Integer(str[i])){
+                    if(number < INT_MAX / 10 || (number == INT_MAX / 10 && (str[i] - '0') <= INT_MAX % 10)){
+                        number = number * 10 + (str[i] - '0');
+                        i++;
+                    }
+                    else{
+                        return INT_MAX;
+                    }
+                }
+            }
+            else{
+                return 0;
+            }
+        }
+        return number;
+    }
+    
+    //方法二
+    int StrToInt_2(string str) {
+        if (str.empty()) {
+            return 0;
+        }
 
-    //跳过空格
-    while (str[i] == ' ') {
-        i++;
-    }
-    //判断第一个非空格符是正号
-    if(str[i] == '+')
-    {
-        i++;
-        for(int j = i; j < str.size(); j++)
-        {
-            if(str[j] >= '0' && str[j] <= '9')
-            {
-                if((num > INT_MAX / 10) || ((num == INT_MAX / 10) && ((str[j] - '0') > (INT_MAX % 10))))
-                {
-                    num = INT_MAX;
-                    break;
-                }
-                else
-                {
-                    last_num = num;
-                    num = last_num * 10 + (str[j] - '0');
-                }
-            }
-            else
-            {
-                break;
-            }
+        const int n = int(str.size());
+        const char* cstr = str.c_str();  // 获取字符串的字符数组指针
+        int result = 0;
+        int sign = 1;
+        int i = 0;
+
+        // 跳过无效字符
+        while (i < n && (cstr[i] == ' ' || cstr[i] == '\t' || cstr[i] == '\n')) {
+            i++;
         }
-    }
-    else if (str[i] == '-')//判断第一个非空格符是负号
-    {
-        i++;
-        for(int k = i; k < str.size(); k++)
-        {
-            if(str[k] >= '0' && str[k] <= '9')
-            {
-                if((-num < INT_MIN / 10) || ((-num == INT_MIN / 10) && (-(str[k] - '0') < (INT_MIN % 10))))
-                {
-                    num = -INT_MIN;
-                    break;
-                }
-                else
-                {
-                    last_num = num;
-                    num = last_num * 10 + (str[k] - '0');
-                }
-            }
-            else
-            {
-                break;
-            }
+
+        // 处理符号位
+        if (i < n && (cstr[i] == '-' || cstr[i] == '+')) {
+            sign = (cstr[i++] == '-') ? -1 : 1;
         }
-        num = -1 * num;
-    }
-    else//判断第一个非空格符是数字/其他
-    {
-        for(int pos = i; pos < str.size(); pos++)
-        {
-            if(str[pos] >= '0' && str[pos] <= '9')
-            {
-                if((num > INT_MAX / 10) || ((num == INT_MAX / 10) && ((str[pos] - '0') > (INT_MAX % 10))))
-                {
-                    num = INT_MAX;
-                    break;
-                }
-                else
-                {
-                    last_num = num;
-                    num = last_num * 10 + (str[pos] - '0');
-                }
-            }
-            else
-            {
-                break;
-            }
+
+        // 预先计算字符到数字的映射
+        int* digits = new int[10];  // 数字映射表
+        for (int j = 0; j < 10; j++) {
+            digits[j] = j;
         }
+
+        // 将字符转换为数字，并累加到结果中
+        while (i < n && isdigit(cstr[i])) {
+            int digit = digits[cstr[i] - '0'];
+            if (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10)) {
+                delete[] digits;
+                return (sign == 1) ? INT_MAX : INT_MIN;
+            }
+            result = result * 10 + digit;
+            i++;
+        }
+
+        delete[] digits;
+        return sign * result;
     }
-    return num;
-}
+};
