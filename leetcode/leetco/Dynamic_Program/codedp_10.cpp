367a7f74cee166ad784345ae3a8b9999c45c7569
ZhouLiqian <96431886+ZhouLiqian@users.noreply.github.com>
2023-08-01 21:49:14 +0800

    add code

--- leetcode/leetco/Dynamic_Program/codedp_10.cpp
+++ leetcode/leetco/Dynamic_Program/codedp_10.cpp
@@ -11,15 +11,18 @@ using namespace::std;
 class Solution {
 public:
     vector<double> dicesProbability(int n) {
-        vector<double> dp(6, 1.0 / 6.0);
-        for (int i = 2; i <= n; i++) {
-            vector<double> tmp(5 * i + 1, 0);
-            for (int j = 0; j < dp.size(); j++) {
-                for (int k = 0; k < 6; k++) {
-                    tmp[j + k] += dp[j] / 6.0;
+        if(n == 0) return {};
+        //i = 1
+        vector<double> dp(6, 1.0/6.0);
+        for(int i = 2; i <= n; i++){
+            vector<double> temp(i*5+1, 0);
+            for(int j = 0; j < dp.size(); j++){
+                for(int k = 0; k < 6; k++){
+                    //独立事件 P(AB) = P(A) * P(B)
+                    temp[j + k] += dp[j] * 1.0/6.0;
                 }
             }
-            dp = tmp;
+            dp = temp;
         }
         return dp;
     }
--- leetcode/leetco/Dynamic_Program/codedp_9.cpp
+++ leetcode/leetco/Dynamic_Program/codedp_9.cpp
@@ -6,20 +6,20 @@
 #include <iostream>
 using namespace::std;
 
-/*我们把只包含质因子 2、3 和 5 的数称作丑数（Ugly Number）。求按从小到大的顺序的第 n 个丑数 解法：动态规划*/
+/*我们把只包含质因子 2、3 和 5 的数称作丑数。求按从小到大的顺序的第 n 个丑数 解法：动态规划*/
 
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        int a = 0, b = 0, c = 0;
+        if(n == 1) return 1;
         int dp[n];
         dp[0] = 1;
-        for(int i = 1; i < n; i++) {
-            int n2 = dp[a] * 2, n3 = dp[b] * 3, n5 = dp[c] * 5;
-            dp[i] = min(min(n2, n3), n5);
-            if(dp[i] == n2) a++;
-            if(dp[i] == n3) b++;
-            if(dp[i] == n5) c++;
+        int a = 0, b = 0, c = 0;
+        for(int i = 1; i < n; i++){
+            dp[i] = min(min(dp[a] * 2, dp[b] * 3), dp[c] * 5);
+            if(dp[i] == dp[a] * 2) a++;
+            if(dp[i] == dp[b] * 3) b++;
+            if(dp[i] == dp[c] * 5) c++;
         }
         return dp[n - 1];
     }
