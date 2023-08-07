cfd328d23da90ca7627b6836b73ac18afcb09518
ZhouLiqian <96431886+ZhouLiqian@users.noreply.github.com>
2023-08-07 17:32:53 +0800

    add code

---
+++ hot100/hot100/Hash_Table/hotht_1.cpp
@@ -0,0 +1,26 @@
+//  hotht_1.cpp
+//  hot100
+//  两数之和
+//  Created by Qian on 8/7/23.
+
+#include <iostream>
+#include <unordered_map>
+using namespace::std;
+
+/*给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出和为目标值 target 的那两个整数，并返回它们的数组下标 解法：哈希*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hashtable;
+        for (int i = 0; i < nums.size(); ++i) {
+            //逆向思维
+            auto it = hashtable.find(target - nums[i]);
+            if (it != hashtable.end()) {
+                return {it->second, i};
+            }
+            hashtable[nums[i]] = i;
+        }
+        return {};
+    }
+};
