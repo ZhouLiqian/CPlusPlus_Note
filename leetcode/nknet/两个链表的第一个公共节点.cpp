5094126c9332e5b381bc1353b85e58c1e8328ef7
ZhouLiqian <96431886+ZhouLiqian@users.noreply.github.com>
2023-08-25 13:54:49 +0800

    change code

--- leetcode/nknet/两个链表的第一个公共节点.cpp
+++ leetcode/nknet/两个链表的第一个公共节点.cpp
@@ -15,47 +15,6 @@ struct ListNode
 
 class Solution {
 public:
-    
-    ListNode *getIntersectionNode_1(ListNode *headA, ListNode *headB) {
-        //分别统计两链表节点个数
-        ListNode* cur_A = headA;
-        int count_A = 0;
-        while(cur_A) {
-            count_A++;
-            cur_A = cur_A -> next;
-        }
-        ListNode* cur_B = headB;
-        int count_B = 0;
-        while(cur_B) {
-            count_B++;
-            cur_B = cur_B -> next;
-        }
-        
-        //删除多余节点
-        if(count_A > count_B){
-            int more = count_A - count_B;
-            while(more){
-                headA = headA -> next;
-                more--;
-            }
-        }
-        else if(count_B > count_A){
-            int more = count_B - count_A;
-            while(more) {
-                headB = headB -> next;
-                more--;
-            }
-        }
-        
-        //遍历节点
-        while (headA && headB) {
-            if(headA == headB) return headA;
-            headA = headA -> next;
-            headB = headB -> next;
-        }
-        return NULL;
-    }
-    
     //双指针
     ListNode *getIntersectionNode_2(ListNode *headA, ListNode *headB){
         ListNode* A = headA;
--- leetcode/nknet/二叉树的最近公共节点.cpp
+++ leetcode/nknet/二叉树的最近公共节点.cpp
@@ -16,43 +16,11 @@ struct TreeNode {
 
 class Solution {
 public:
-    //哈希表
-    unordered_map<int, TreeNode*> tree;
-    unordered_map<int, bool> check;
-    void dfs(TreeNode* root){
-        if(root -> left){
-            tree[root -> left -> val] = root;
-            dfs(root -> left);
-        }
-        if(root -> right){
-            tree[root -> right -> val] = root;
-            dfs(root -> right);
-        }
-    }
-    TreeNode* lowestCommonAncestor_1(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(!root) return root;
-        
-        //父子关系建立哈希表
-        tree[root -> val] = NULL;
-        dfs(root);
-        
-        //遍历表
-        while (p) {
-            check[p -> val] = true;
-            p = tree[p -> val];
-        }
-        while (q) {
-            if(check[q -> val]) return q;
-            q = tree[q -> val];
-        }
-        return NULL;
-    }
-    
     //先序遍历
     TreeNode* lowestCommonAncestor_2(TreeNode* root, TreeNode* p, TreeNode* q){
         if(root == NULL || root == p || root == q) return root;
-        TreeNode* left = lowestCommonAncestor_1(root -> left, p, q);
-        TreeNode* right = lowestCommonAncestor_1(root -> right, p, q);
+        TreeNode* left = lowestCommonAncestor_2(root -> left, p, q);
+        TreeNode* right = lowestCommonAncestor_2(root -> right, p, q);
         if(left == NULL) return right;
         if(right == NULL) return left;
         return root;
