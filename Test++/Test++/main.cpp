19c98f0850edaff18eded797fe26aa1aa6925f3a
ZhouLiqian <96431886+ZhouLiqian@users.noreply.github.com>
2023-09-02 22:16:55 +0800

    add code

--- Test++/Test++/main.cpp
+++ Test++/Test++/main.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <vector>
 
+int main() {
+    std::vector<int> dynamicArray; // 使用 std::vector 存储动态数组
+
+    int input;
+    while (std::cin >> input) {
+        dynamicArray.push_back(input); // 将输入添加到动态数组中
+    }
+
+    // 现在 dynamicArray 包含了所有用户输入的整数
+
+    // 可以遍历 dynamicArray 并处理数据
+    for (int number : dynamicArray) {
+        std::cout << number << " ";
+    }
+
+    return 0;
+}
