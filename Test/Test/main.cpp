15767a7fc12f4d48cc3b4513d5370524514a3d02
ZhouLiqian <96431886+ZhouLiqian@users.noreply.github.com>
2023-09-20 19:01:23 +0800

    add code

--- Test/Test/main.cpp
+++ Test/Test/main.cpp
@@ -1,14 +1,1 @@
-#include <iostream>
-using namespace::std;
 
-struct xxx{
-    double val; //8
-    double arg; //8
-    double num; //8
-    char sym; //1
-};
-
-int main(){
-    cout << sizeof(xxx) << endl;
-    return 0;
-}
